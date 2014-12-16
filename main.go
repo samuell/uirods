8dc22282d607f268a309cd2dcfd74feb7970539a
Samuel Lampa <samuel.lampa@gmail.com>
2014-12-16 18:06:27 +0100

    Now very simply parses and lists output from ils

--- main.go
+++ main.go
@@ -1,15 +1,25 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"github.com/samuell/glow"
+	"net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "<h1>iRODS Thin UI</h1><p>Welcome %s!</p>", r.URL.Path[1:])
+	fmt.Fprintf(w, "<h1>iRODS Thin UI</h1><p>Welcome user %s!</p>", r.URL.Path[1:])
+	cmdIn := make(chan string, 0)
+	linesOut := make(chan []byte, 16)
+	glow.NewCommandExecutor(cmdIn, linesOut)
+	cmdIn <- "ils"
+	fmt.Fprint(w, "<ul>")
+	for line := range linesOut {
+		fmt.Fprint(w, "<li>", string(line), "</li>")
+	}
+	fmt.Fprint(w, "</ul>")
 }
 
 func main() {
-    http.HandleFunc("/", handler)
-    http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
