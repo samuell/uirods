36a74c9d17c1c63c92bd8099dcc84bbfb9b8ffd8
Samuel Lampa <samuel.lampa@gmail.com>
2014-12-19 16:17:32 +0100

    Combine with a static file server

--- main.go
+++ main.go
@@ -6,12 +6,15 @@ import (
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func irodsPathHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>iRODS Thin UI</h1><p>Welcome user %s!</p>", r.URL.Path[1:])
+
 	cmdIn := make(chan string, 0)
 	linesOut := make(chan []byte, 16)
+
 	glow.NewCommandExecutor(cmdIn, linesOut)
 	cmdIn <- "ils"
+
 	fmt.Fprint(w, "<ul>")
 	for line := range linesOut {
 		fmt.Fprint(w, "<li>", string(line), "</li>")
@@ -20,6 +23,10 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	// Handle iRODS paths (for showing metadata etc)
+	http.HandleFunc("/irods/", irodsPathHandler)
+	// Handle file system paths
+	http.Handle("/", http.FileServer(http.Dir(".")))
+	// Serve on port 8080
 	http.ListenAndServe(":8080", nil)
 }
