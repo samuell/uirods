afa587b833a7fc9b5be7d9c6ec9d514597314e9f
Samuel Lampa <samuel.lampa@gmail.com>
2014-12-19 19:06:24 +0100

    Folder navigation now fully functional

--- main.go
+++ main.go
@@ -4,11 +4,22 @@ import (
 	"bytes"
 	"fmt"
 	"github.com/samuell/glow"
+	"log"
 	"net/http"
+	"os/exec"
+	"strings"
 )
 
 func irodsPathHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>iRODS Thin UI</h1><p>Welcome user %s!</p>", r.URL.Path[1:])
+	fmt.Fprint(w, "<h1>iRODS Thin UI</h1>")
+
+	targetFolder := strings.Replace(r.URL.RequestURI(), "/irods", "", 1)
+
+	cmd := exec.Command("icd", targetFolder)
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	cmdIn := make(chan string, 0)
 	linesOut := make(chan []byte, 16)
@@ -18,13 +29,14 @@ func irodsPathHandler(w http.ResponseWriter, r *http.Request) {
 
 	cnt := 0
 	for line := range linesOut {
+		line := bytes.Replace(line, []byte("  C- "), []byte(""), 1)
+		pathParts := bytes.Split(line, []byte("/"))
 		if cnt > 0 {
-			line := bytes.Replace(line, []byte("  C- "), []byte(""), 1)
-			pathParts := bytes.Split(line, []byte("/"))
 			folderName := pathParts[len(pathParts)-1]
 			fmt.Fprint(w, "<li><a href=\"/irods", string(line), "\">", string(folderName), "</a></li>")
 		} else {
 			fmt.Fprint(w, "<p>Current folder: ", string(line), "</p>")
+			fmt.Fprint(w, "<p><a href=\"/irods", string(bytes.Join(pathParts[:len(pathParts)-1], []byte("/"))), "\">Parent folder</a></p>")
 			fmt.Fprint(w, "<ul>")
 		}
 		cnt++
