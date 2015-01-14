eec6fa30cb3cfde31751a1fbb3714b6ca0e182ed
Samuel Lampa <samuel.lampa@gmail.com>
2015-01-14 12:11:35 +0100

    Re-use environment variables in run script

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export IRODSMNT_IRODSPATH=/
 export IRODSMNT_FILESPATH=~/mnt/irods
-icd /
-irodsFs ~/mnt/irods
+icd $IRODSMNT_IRODSPATH
+irodsFs $IRODSMNT_IRODSPATH
 go run main.go -p 8081
